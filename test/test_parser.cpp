ba74a059d1b1a602acb8281ffdea07832fe4b11f
Florian Limberger <flo@snakeoilproductions.net>
2017-02-11 14:39:34 +0100

    Add (failing) test for feed parser

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -4,7 +4,9 @@
 
 #include <QtCore/QBuffer>
 #include <QtCore/QByteArray>
+#include <QtCore/QCoreApplication>
 #include <QtCore/QString>
+#include <QtCore/QTimer>
 #include <QtCore/QUrl>
 
 #include <Entry.hpp>
@@ -34,10 +36,25 @@ TEST(test_parser, test_basic_parsing) {
     };
     QBuffer buffer{};
     FeedParser parser{feed, &buffer};
-    buffer.setData(data);
+    bool parser_success = false;
+    QObject::connect(&parser, &FeedParser::done,
+                     [&parser_success](bool success, const std::shared_ptr<Feed> &feed) {
+        parser_success = success;
+    });
+    QTimer::singleShot(0, [&data, &buffer]() { buffer.setData(data); });
+    ASSERT_TRUE(parser_success);
     ASSERT_EQ(feed->size(), 1);
     const auto entry = feed->getEntry(0);
     ASSERT_STREQ(str(entry->title()), "Simple Title");
     ASSERT_STREQ(str(entry->dateTime().toString()), "2017-02-10T22:51");
     ASSERT_STREQ(str(entry->content()), "A simple test entry");
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    QCoreApplication app{argc, argv};
+    RUN_ALL_TESTS();
+    QTimer::singleShot(0, []() { qApp->exit(); });
+    return app.exec();
 }
