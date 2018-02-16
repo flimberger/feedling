2b960d1e0a74f1c19262f91bb470c582b06ca338
Florian Limberger <flo@florian-limberger.de>
2018-02-17 00:31:14 +0100

    Parse common Atom constructs according to the spec

--- src/AtomParser.cpp
+++ src/AtomParser.cpp
@@ -7,6 +7,18 @@
 
 namespace feedling {
 
+Text::Text() : type{Plain}, text{} {}
+
+Text::Text(Type type, QString text) : type{type}, text{std::move(text)} {}
+
+Person::Person() = default;
+
+Person::Person(Text name, QUrl uri, QString email)
+    : name{std::move(name)},
+      uri{std::move(uri)},
+      email{std::move(email)}
+{}
+
 AtomParser::AtomParser(const std::shared_ptr<Feed> &feed) : FeedParser{feed} {}
 
 AtomParser::~AtomParser() = default;
@@ -34,29 +46,45 @@ QString AtomParser::errorString() const {
             .arg(m_xmlReader.columnNumber());
 }
 
+QDateTime AtomParser::readDateConstruct() {
+    Q_ASSERT(m_xmlReader.isStartElement());
+
+    auto text = m_xmlReader.readElementText();
+    auto date = QDateTime::fromString(text);
+
+    if (!date.isValid()) {
+        qWarning() << "invalid date:" << text;
+    }
+
+    return date;
+}
+
 void AtomParser::readEntry() {
     Q_ASSERT(m_xmlReader.isStartElement() && m_xmlReader.name() == "entry");
 
-    QString title;
+    Person author;  // currently ignored
+    Text title;  // the text type is currently ignored
     QString content;
     QDateTime date;
 
     while (m_xmlReader.readNextStartElement()) {
         qDebug() << "parsing element" << m_xmlReader.name();
-        if (m_xmlReader.name() == "title") {
-            title = m_xmlReader.readElementText();
+        if (m_xmlReader.name() == "author") {
+            author = readPersonConstruct();
+        } else if (m_xmlReader.name() == "title") {
+            title = readTextConstruct();
         } else if (m_xmlReader.name() == "content") {
             content = m_xmlReader.readElementText();
         } else if (m_xmlReader.name() == "published") {
-            date = QDateTime::fromString(m_xmlReader.readElementText());
+            date = readDateConstruct();
         } else {
             m_xmlReader.skipCurrentElement();
         }
     }
 
-    qDebug() << "parsed entry" << title;
+    qDebug() << "parsed entry" << title.text;
     auto feed = FeedParser::feed();
-    feed->addEntry(std::make_shared<Entry>(title, content, date, feed));
+    feed->addEntry(std::make_shared<Entry>(title.text, content, date, feed));
 }
 
 void AtomParser::readFeed() {
@@ -70,6 +98,46 @@ void AtomParser::readFeed() {
             m_xmlReader.skipCurrentElement();
         }
     }
+}
+
+Person AtomParser::readPersonConstruct() {
+    Q_ASSERT(m_xmlReader.isStartElement());
+
+    Text name;
+    QString uri;
+    QString email;
+
+    while (m_xmlReader.readNextStartElement()) {
+        if (m_xmlReader.name() == "name") {
+            name = readTextConstruct();
+        } else if (m_xmlReader.name() == "uri") {
+            uri = m_xmlReader.readElementText();
+        } else if (m_xmlReader.name() == "email") {
+            email = m_xmlReader.readElementText();
+        }
+    }
+
+    return Person{name, uri, email};
+}
+
+Text AtomParser::readTextConstruct() {
+    Q_ASSERT(m_xmlReader.isStartElement());
+
+    Text::Type type = Text::Plain;
+
+    for (const auto &attribute : m_xmlReader.attributes()) {
+        if (attribute.name() == "type") {
+            if (attribute.value() == "html") {
+                type = Text::HTML;
+            } else if (attribute.value() == "xhtml") {
+                type = Text::XHTML;
+            } else if (attribute.value() != "text") {
+                qWarning() << "ignoring invalid text type" << attribute.value();
+            }
+        }
+    }
+
+    return Text{type, m_xmlReader.readElementText()};
 }
 
 }  // namespace feedling
--- src/AtomParser.hpp
+++ src/AtomParser.hpp
@@ -3,11 +3,33 @@
 
 #include <memory>
 
+#include <QtCore/QUrl>
 #include <QtCore/QXmlStreamReader>
 
 #include "FeedParser.hpp"
 
 namespace feedling {
+
+struct Text {
+    enum Type {
+        Plain,
+        HTML,
+        XHTML
+    } type;  // default value: Plain
+    QString text;  // required
+
+    Text();
+    Text(Type type, QString text);
+};
+
+struct Person {
+    Text name;  // required
+    QUrl uri;  // optional
+    QString email;  // optional
+
+    Person();
+    Person(Text name, QUrl uri, QString email);
+};
 
 // Simple parser which currently only understands Atom
 class AtomParser : public FeedParser
@@ -21,8 +43,11 @@ public:
     QString errorString() const override;
 
 private:
+    QDateTime readDateConstruct();
     void readEntry();
     void readFeed();
+    Person readPersonConstruct();
+    Text readTextConstruct();
 
     QXmlStreamReader m_xmlReader;
 };
