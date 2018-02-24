469327fb2f04d103afe451e2477176c3c97d2b5b
Florian Limberger <flo@purplekraken.com>
2018-02-24 20:32:53 +0100

    Use Content-Type HTTP header to decide wether Atom or RSS is parsed

--- src/Fetcher.cpp
+++ src/Fetcher.cpp
@@ -1,5 +1,8 @@
 #include "Fetcher.hpp"
 
+#include <string_view>
+#include <memory>
+
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtNetwork/QNetworkReply>
 #include <QtNetwork/QNetworkRequest>
@@ -7,8 +10,37 @@
 #include "AtomParser.hpp"
 #include "Feed.hpp"
 #include "FeedsModel.hpp"
+#include "RSSParser.hpp"
 
 namespace feedling {
+namespace {
+
+constexpr bool starts_with(std::string_view str, std::string_view substr) {
+    auto compare = &std::string_view::traits_type::compare;
+
+    return str.size() >= substr.size() && compare(str.data(), substr.data(), substr.size()) == 0;
+}
+
+enum class FeedType {
+    Indeterminate,  // application/xml, text/xml
+    Atom,  // application/atom+xml
+    RSS  // application/rss+xml
+};
+
+constexpr FeedType feedType(std::string_view contentType) {
+    constexpr std::string_view ATOM = "application/atom+xml";
+    constexpr std::string_view RSS = "application/rss+xml";
+
+    if (starts_with(contentType, ATOM)) {
+        return FeedType::Atom;
+    } else if (starts_with(contentType, RSS)) {
+        return FeedType::RSS;
+    }
+
+    return FeedType::Indeterminate;
+}
+
+}  // namespace
 
 Fetcher::Fetcher(FeedsModel *model, QObject *parent)
   : QObject(parent),
@@ -39,9 +71,26 @@ void Fetcher::onFeedDownloadFinished(QNetworkReply *reply)
     auto feed = m_feedsModel->getFeed(url).lock();
     if (feed) {
         // TODO: this should be performed in a separate background thread
-        AtomParser parser{feed};
-        if (!parser.read(reply)) {
-            qWarning() << "failed to parse" << url.toString() << ':' << parser.errorString();
+        auto contentType = reply->rawHeader("Content-Type");
+        qDebug() << "Content-Type:" << contentType;
+        auto type = feedType(contentType.data());
+        std::unique_ptr<FeedParser> parser;
+
+        switch (type) {
+        case FeedType::Atom:
+            parser = std::make_unique<AtomParser>(feed);
+            break;
+        case FeedType::RSS:
+            parser = std::make_unique<RSSParser>(feed);
+            break;
+        default:
+            qWarning() << "indeterminate content:" << url.toString();
+        }
+
+        if (parser) {
+            if (!parser->read(reply)) {
+                qWarning() << "failed to parse" << url.toString() << ':' << parser->errorString();
+            }
         }
     } else {
         qWarning() << "unknown URL:" << url;
